a7599a565d89ccd9e8e5dbb2f2d7e5e4a4b53882
wealthymanyasa <wealthymanyasa@gmail.com>
2024-11-22 16:14:47 +0200

    add verification token

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -1,5 +1,17 @@
 import { db } from "lib/db";
-
+//getVerificationToken by token
+export const getVerificationTokenByToken = async (token: string) => {
+    try {
+        const verificationToken = await db.verificationToken.findFirst({
+            where: { token }
+        });
+        return verificationToken;
+        
+    } catch {
+        return null;
+    }
+}
+//getVerificationTokenByEmail
 export const getVerificationTokenByEmail = async (email: string) => {
     try {
         const verificationToken = await db.verificationToken.findFirst({
