a68b8869d0d0a6b53fd520e8cc2bf2944bbd51fe
wealthymanyasa <wealthymanyasa@gmail.com>
2024-11-22 14:50:54 +0200

    finish OAuth login

---
+++ data/verification-token.ts
@@ -0,0 +1,13 @@
+import { db } from "lib/db";
+
+export const getVerificationTokenByEmail = async (email: string) => {
+    try {
+        const verificationToken = await db.verificationToken.findFirst({
+            where: { email }
+        });
+        return verificationToken;
+        
+    } catch {
+        return null;
+    }
+}
