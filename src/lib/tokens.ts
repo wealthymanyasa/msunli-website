0325485bca96e08a461ac8ab230c4ded7f222bff
wealthymanyasa <wealthymanyasa@gmail.com>
2024-11-26 15:32:36 +0200

    generate tokens

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,5 +1,36 @@
-import { v4 } from "uuid"
+import { getVerificationTokenByEmail } from "data/verification-token";
+import { v4 as uuidv4 } from "uuid"
+import { db } from "lib/db";
 
+
+/**
+ * All kinds of tokens to be generated
+*/
 export const generateVerificationToken = async (email: string) => {
+    //ensure uniqui token
+    const token = uuidv4();
+    //set expires 
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getVerificationTokenByEmail(email);
+    //check db for verification token
+    if (existingToken) {
+        await db.verificationToken.delete({
+            where: {
+                id: existingToken.id
+            }
+        });
+    }
+
+    // generate verificationToken
+    const verificationToken = await db.verificationToken.create({
+        data: {
+            email,
+            token,
+            expires
+        }
+    });
+
+    return verificationToken;
 
 }
